66af660711378aec47f2a8acd58f8a3e4be7ffef
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-02-17 13:27:13 -0500

    Makes 3 hash items for my store

---
+++ store_item.rb
@@ -0,0 +1,4 @@
+#represent items in my store as a hash
+shirt = [size: "medium", color: "red", style: "t-shirt"]
+skirt = [size: "small", color: "purple", style: "mini"]
+pants = [size: 6, color: "black", style: "skinny"]
