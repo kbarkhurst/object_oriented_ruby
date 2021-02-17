d742cb9b06ba7dad929b6a13f822f46f5262bfad
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-02-17 14:38:23 -0500

    Rewrites store as class

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,50 @@
 #represent items in my store as a hash
 shirt = [size: "medium", color: "red", style: "t-shirt"]
 skirt = [size: "small", color: "purple", style: "mini"]
-pants = [size: 6, color: "black", style: "skinny"]
+pants = [size: "six", color: "black", style: "skinny"]
+
+class Product
+  def initialize(input_name, input_size, input_color, input_style)
+    @name = input_name
+    @size = input_size
+    @color = input_color
+    @style = input_style
+  end
+
+  def size
+    @size
+  end
+
+  def color
+    @color
+  end
+
+  def color
+    @color
+  end
+
+  def name=(input_name)
+    @name = input_name
+  end
+
+  def size=(input_size)
+    @size = input_size
+  end
+
+  def color=(input_color)
+    @color = input_color
+  end
+
+  def style=(style_size)
+    @style = style_size
+  end
+end
+
+shirt = Product.new("shirt", "medium", "red", "t-shirt")
+skirt = Product.new("skirt", "small", "purple", "mini")
+pants = Product.new("pants", "six", "black", "skinny")
+p shirt
+p skirt
+p pants
+shirt.color = ("purple")
+p shirt
